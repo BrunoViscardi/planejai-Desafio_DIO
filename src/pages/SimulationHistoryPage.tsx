1913f4804e2185d9a7d9d0f719fcae43ffea9cc1
Bruno Viscardi <bruno.viscardi33@gmail.com>
2026-07-29 15:17:49 -0300

    feat: adiciona o historico a página de forma inicial

--- src/pages/SimulationHistoryPage.tsx
+++ src/pages/SimulationHistoryPage.tsx
@@ -1,7 +1,11 @@
 import { Card } from "../components/features/History/HistoryCard";
 import { PageHero } from "../components/shared/PageHero";
+import { useSimulationStorage } from "../hooks/useSimulationStorage";
 
 export function SimulationHistoryPage() {
+  const { getFormData } = useSimulationStorage();
+  const data = getFormData();
+
   return (
     <main className="mx-auto max-w-6xl px-4 py-10 sm:py-14">
       <PageHero
@@ -9,23 +13,21 @@ export function SimulationHistoryPage() {
         subtitle="Acompanhe o histórico de seus planos financeiros."
       />
 
-      <div>
-        <Card
-          goalName="Meta de Compras"
-          goalDate="01/01/2023"
-          goalAmount="R$ 1.000,00"
-          goalDeadline="31/12/2023"
-          monthlySavings="R$ 83,33"
-        />
-
-        <Card
-          goalName="Meta de compra no Carrefour"
-          goalDate="01/01/2023"
-          goalAmount="R$ 100.000.000,00"
-          goalDeadline="31/12/2023"
-          monthlySavings="R$ 83,33"
-        />
-      </div>
+      {!data ? (
+        <p> O histórico de simulações está vazio.</p>
+      ) : (
+        data.map((item, index) => (
+          <div key={index}>
+            <Card
+              goalName={item.goalName}
+              goalDate={item.goalDate}
+              goalAmount={`R$ ${item.goalAmount}`}
+              goalDeadline={`${item.goalDeadline} meses`}
+              monthlySavings={`R$ ${item.monthlySavings}`}
+            />
+          </div>
+        ))
+      )}
     </main>
   );
 }
