8231489ccf15823d95a4b8d1a1caec2460d5f1f2
Bruno Viscardi <bruno.viscardi33@gmail.com>
2026-07-30 16:23:53 -0300

    style: tornando o cardHistory responsivo

--- src/components/features/History/HistoryCard.tsx
+++ src/components/features/History/HistoryCard.tsx
@@ -33,19 +33,19 @@ export function Card({
   return (
     <div
       className={
-        "bg-card mb-6 flex items-center w-full justify-between  rounded-2xl p-6  shadow-[4px_4px_18px_0px_rgba(0,0,0,0.2)]"
+        "bg-card mb-6 gap-2 flex w-full flex-col rounded-2xl p-6 shadow-[4px_4px_18px_0px_rgba(0,0,0,0.2)] lg:flex-row lg:items-center lg:justify-between"
       }
     >
       <div className="flex h-10 w-10  shrink-0 items-center justify-center rounded-[10.67px] bg-[#ECE5F8]">
         <Goal size={28} className="text-primary" />
       </div>
 
-      <div className="w-[210px]">
+      <div className="w-full lg:w-[210px]">
         <span className={styleClasses.goalName}>{goalName}</span>
         <p className={styleClasses.createdAt}>{createdAt}</p>
       </div>
 
-      <div className="w-[150px]">
+      <div className="w-full lg:w-[150px]">
         <span className={styleClasses.label}>Custo da Meta</span>
         <p className={styleClasses.value}>{goalAmount}</p>
       </div>
@@ -55,14 +55,18 @@ export function Card({
         <p className={styleClasses.value}>{goalDeadline}</p>
       </div>
 
-      <div className="w-[150px]">
+      <div className="w-full lg:w-[150px]">
         <span className={styleClasses.label}>Economia Mensal</span>
         <p className={styleClasses.value}>{monthlySavings}</p>
       </div>
 
-      <div className="flex items-center gap-2">
-        <Divider orientation="vertical" />
+      {/* Apenas desktop */}
+      <Divider orientation="vertical" className="hidden lg:block" />
 
+      {/* Apenas mobile */}
+      <Divider orientation="horizontal" className="block lg:hidden" />
+
+      <div className="flex justify-evenly gap-6">
         <Button
           type="button"
           icon={Trash2}
@@ -70,6 +74,8 @@ export function Card({
           variant="ghost"
           onClick={() => onDelete(id)}
         ></Button>
+
+        <Divider orientation="vertical" className="lg:hidden" />
 
         <Button
           type="button"
